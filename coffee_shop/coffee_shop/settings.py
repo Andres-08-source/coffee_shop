e36f1a1448094ad80d09a0f8ed4fdbc3ead6dff2
Andres <andresleguizamon088@gmail.com>
2025-04-16 23:52:40 -0500

    se agreagaron cosas como estilos, creacion del formulario, lista de productos, uso y modificacion de los templates, y demas modificaciones

--- coffee_shop/coffee_shop/settings.py
+++ coffee_shop/coffee_shop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
-    
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'coffee_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- coffee_shop/products/models.py
+++ coffee_shop/products/models.py
@@ -5,7 +5,7 @@ class Product (models.Model):
     name = models.CharField (max_length=200, verbose_name="nombre")
     description = models.CharField(max_length=500, verbose_name="descripcion")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
-    available = models.BooleanField(default=True, verbose_name="disponible")
+    avaliable = models.BooleanField(default=True, verbose_name="disponible")
     photo = models.ImageField(upload_to="logos", null=True, blank=True, verbose_name="foto")
     
     def __str__(self):
